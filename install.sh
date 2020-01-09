d2fd59aa23fb37e95a0904fb66c748cd524b3eb9
jhover <jhover@transit.cshl.edu>
2020-01-09 16:18:47 -0500

    Update.

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ cd /etc/yum.repos.d
 wget https://research.cs.wisc.edu/htcondor/yum/RPM-GPG-KEY-HTCondor
 rpm --import RPM-GPG-KEY-HTCondor
 wget https://research.cs.wisc.edu/htcondor/yum/repo.d/htcondor-stable-rhel7.repo
+sleep 5
 yum install condor
 sleep 5
 cd /etc/condor/config.d
@@ -12,7 +13,7 @@ mv * unused
 cp ~/git/rugen-config/etc/condor_config /etc/condor_config
 cp ~/git/rugen-config/etc/config.d/* /etc/condor/config.d/
 sleep 5
-firewall-cmd --premanent --add-port=9618/tcp
+firewall-cmd --permanent --add-port=9618/tcp
 service firewalld restart
 firewall-cmd --list-all
 echo "Done. Place password file and start..."
